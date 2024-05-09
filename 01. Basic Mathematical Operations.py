f3d55e2a85fe39c61e0bbd67a658eee5071703ba
vedant <vedantg.r10@gmail.com>
2024-05-09 17:02:39 +0530

    Basic Math Ops

---
+++ 01. Basic Mathematical Operations.py
@@ -0,0 +1,26 @@
+#BASIC MATHEMATICAL OPERATIONS
+x=2
+x=x+2
+print('Addition:',x) #ADDITION
+
+y=5
+y=y-2
+print('Subtraction:',y) #SUBTRACION
+
+z=2
+z=z*2
+print('Multiplication:',z) #MULTIPLICATION
+
+a=2
+a=a**2
+print('Square:',a) #SQUARE
+
+b=6
+b=b/5
+print('Division:',b) #DIVISION
+
+c=7
+c=c%2
+print('Remainder:',c) #REMAINDER
+
+
