e66bc6aa7650930c0e7eeaec4e5f52e7eed2f126
vedant <vedantg.r10@gmail.com>
2024-05-15 03:00:24 +0530

    Python I/O Exercises

---
+++ 36. Accept any three strings from one input func call.py
@@ -0,0 +1,4 @@
+Name1, Name2, Name3 = input('Enter three strings: ').split()
+print('Name1:', Name1)
+print('Name2:', Name2)
+print('Name3:', Name3)
---
+++ 37. Format variables using a string.formamt func method.py
@@ -0,0 +1,5 @@
+totalmon = 1000
+quantity = 3
+price = 450
+
+print('I have {0} dollars so I can buy {1} football for {2} dollars.'.format(totalmon, quantity, price))
